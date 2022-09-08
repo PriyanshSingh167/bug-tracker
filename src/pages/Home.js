7a906454f1aed231187d04734a0cffb6a9275856
priyanshsingh <priyansh@singh.com>
2022-09-08 14:43:56 +0530

    adding users to firestore

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,6 +6,7 @@ import {
   updateProfile,
 } from "firebase/auth";
 import { db } from "../firebase.config";
+import { doc, setDoc, serverTimestamp } from "firebase/firestore";
 import { ReactComponent as ArrowRightIcon } from "../assets/svg/keyboardArrowRightIcon.svg";
 import visibilityIcon from "../assets/svg/visibilityIcon.svg";
 
@@ -39,6 +40,12 @@ function Home() {
       );
 
       const user = userCredential.user;
+
+      const formDataCopy = { ...formData };
+      delete formDataCopy.password;
+      formDataCopy.timestamp = serverTimestamp();
+
+      await setDoc(doc(db, "users", user.uid), formDataCopy);
 
       updateProfile(auth.currentUser, {
         displayName: name,
