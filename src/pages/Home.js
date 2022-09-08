d51b69da032021b6b39626442ef45f2f3accfb2e
priyanshsingh <priyansh@singh.com>
2022-09-08 13:59:04 +0530

    Register user

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,11 @@
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  updateProfile,
+} from "firebase/auth";
+import { db } from "../firebase.config";
 import { ReactComponent as ArrowRightIcon } from "../assets/svg/keyboardArrowRightIcon.svg";
 import visibilityIcon from "../assets/svg/visibilityIcon.svg";
 
@@ -20,6 +26,29 @@ function Home() {
       [e.target.id]: e.target.value,
     }));
   };
+  const onSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const auth = getAuth();
+
+      const userCredential = await createUserWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+
+      const user = userCredential.user;
+
+      updateProfile(auth.currentUser, {
+        displayName: name,
+      });
+
+      navigate("/mark");
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
     <>
       <div className="split left">
@@ -33,7 +62,7 @@ function Home() {
               <p className="pageHeader">Welcome Back!</p>
             </header>
 
-            <form>
+            <form onSubmit={onSubmit}>
               <input
                 type="text"
                 className="nameInput"
