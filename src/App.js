05297c7757ef1611a5f876133e9a08ee7c1c0b3c
priyanshsingh <priyansh@singh.com>
2022-09-10 22:50:32 +0530

    Private route and authStatus hook

--- src/App.js
+++ src/App.js
@@ -5,15 +5,24 @@ import Home from "./pages/Home";
 import SignUp from "./pages/SignUp";
 import Login from "./pages/Login";
 import Profile from "./pages/Profile";
+import PrivateRoute from "./components/PrivateRoute";
 function App() {
   return (
     <>
       <Router>
         <Routes>
-          <Route path="/home" element={<Home />} />
+          {/* Sign Up */}
           <Route path="/" element={<SignUp />} />
+          {/* Sign-in */}
           <Route path="/login" element={<Login />} />
-          <Route path="/profile" element={<Profile />} />
+          {/* Profile(Protected) */}
+          <Route path="/profile" element={<PrivateRoute />}>
+            <Route path="/profile" element={<Profile />} />
+          </Route>
+          {/* Home (Protected) */}
+          <Route path="/home" element={<PrivateRoute />}>
+            <Route path="/home" element={<Home />} />
+          </Route>
         </Routes>
       </Router>
       <ToastContainer />
